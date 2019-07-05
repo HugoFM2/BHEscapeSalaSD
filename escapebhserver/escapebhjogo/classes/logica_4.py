85ac6afd6ad48bc8cf3b834bfa9dd65fef3ca474
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2019-07-05 16:41:55 -0300

    Reescrita da Logica 4

--- escapebhserver/escapebhjogo/classes/logica_4.py
+++ escapebhserver/escapebhjogo/classes/logica_4.py
@@ -2,127 +2,72 @@ import RPi.GPIO as GPIO # Modulo de controle da GPIOs
 import time # Modulo para delays e contagem de tempo
 import threading # Modulo para trabalhar com treads
 from escapebhjogo.classes.mcp23017 import MCP23017 as mcp # Classe para trabalhar com o MCP23017, referenciada como mcp
-from escapebhjogo.classes.logica_1 import Logica_1 # Classe com metodos da logica 1
-from escapebhjogo.classes.logica_2 import Logica_2 # Classe com metodos da logica 2
-from escapebhjogo.classes.logica_3 import Logica_3 # Classe com metodos da logica 3
+from escapebhjogo.classes.logica_3 import Logica_3 # Classe com metodos da logica 2
+from .logica_geral import Logica_geral
 
 """ CLASSE LOGICA 4
 Esta classe faz todo o controle dos itens relacionados a Logica 4
-"""
-class Logica_4(object):
-    # ATRIBUTOS DA CLASSE
-    concluida = False # Atributo que guarda se a logica foi concluida
-    leituraSensores = [] # Atributo que armazena leitura dos sensores
-    tempo_inicial = None # Marca o tempo de inicio da Logica
-    duracao_total = None # Guarda o tempo total da logica
-    t = None # Variavel que ira armazenar a Thread da classe
 
+Laranja -> Virando as duas chaves no móvel que fica no mezanino vai fazer com que o busto
+abra. Dentro do busto vai ter um botão. Apertando o botão no busto vai fazer com que a porta
+secreta abra.
+"""
+class Logica_4(Logica_geral):
+    
+    # GPIO's
+    gpio_chave1 = 0 # Primeira chave (raspberry)
+    gpio_chave2 = 0 # Segunda chave (raspberry)
+    gpio_botao = 0 # Botao no busto (raspberry)
+    gpio_servo = 0 # Servo motor do busto
+    gp_porta = 0 # Rele da trava do alçapão (extensor)
+
+    # Sobreescrevendo metodo setup() da classe pai
     @classmethod
     def setup(cls):
-        # Esta Logica usa GPIOS do raspberry para leitura das chaves(alavancas)
-        # MAPEAMENTO
-        # 1ª Chave - Pino 15 Raspberry
-        # 2ª Chave - Pino 16 Raspberry
-        # 3ª Chave - Pino 37 Raspberry
-        # 4ª Chave - Pino 13 Raspberry
         GPIO.setmode(GPIO.BOARD) # Contagem de (0 a 40)
         GPIO.setwarnings(False) # Desativa avisos
-        # Configurando SENSORES como INPUT em Pull_Down
-        GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-        GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-        GPIO.setup(37, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-        GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        
+        # Configurado GPIO's do raspberry
+        GPIO.setup(cls.gpio_chave1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        GPIO.setup(cls.gpio_chave2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        # GPIO SERVO ?
 
-        # GPA4 - 0x24(ADDRESS2) -> Teto
-        # Pino TETO COMO OUT e inicialmente em nivel baixo
-        mcp.setup(4, mcp.GPA, mcp.OUT, mcp.ADDRESS2)
-        mcp.setup(5, mcp.GPA, mcp.OUT, mcp.ADDRESS2)
-        mcp.output(4, mcp.GPA, mcp.LOW, mcp.ADDRESS2)
-        mcp.output(5, mcp.GPA, mcp.LOW, mcp.ADDRESS2)
-    
-    @classmethod
-    def getLeituraSensores(cls):
-        return cls.leituraSensores
+        # Configurando GPIO's do Extensor
+        mcp.setup(cls.gp_porta, mcp.GPA, mcp.OUT, mcp.ADDRESS2)
 
-    @classmethod
-    def getDuracaoLogica(cls):
-        duracao = 0
-        if cls.tempo_inicial != None and cls.duracao_total == None:
-            duracao = round(time.time() - cls.tempo_inicial , 2) # Arredonda para duas casa decimais
-        elif cls.duracao_total != None:
-            duracao = round(cls.duracao_total,2) # Arredonda para duas casa decimais
-        else:
-            duracao = 0
-        return duracao
+        # Inicialmente em nivel baixo
+        mcp.output(cls.gp_porta, mcp.GPA, mcp.LOW, mcp.ADDRESS2)
 
+    # Metodo para destravar alcapao no chao
     @classmethod
-    def forcarAbrirTeto(cls):
+    def abrirPorta(cls):
         cls.concluida = True
-        cls.duracao_total = time.time() - cls.tempo_inicial
-        mcp.setup(4, mcp.GPA, mcp.OUT, mcp.ADDRESS2)
-        mcp.setup(5, mcp.GPA, mcp.OUT, mcp.ADDRESS2)
-        mcp.output(4, mcp.GPA, mcp.HIGH, mcp.ADDRESS2)
-        mcp.output(5, mcp.GPA, mcp.HIGH, mcp.ADDRESS2)
-        time.sleep(2)
-        mcp.output(4, mcp.GPA, mcp.LOW, mcp.ADDRESS2)
-        mcp.output(5, mcp.GPA, mcp.LOW, mcp.ADDRESS2)
+        mcp.setup(cls.gp_porta, mcp.GPA, mcp.OUT, mcp.ADDRESS2)
+        mcp.output(cls.gp_porta, mcp.GPA, mcp.HIGH, mcp.ADDRESS2)
+        time.sleep(30)
+        mcp.output(cls.gp_porta, mcp.GPA, mcp.LOW, mcp.ADDRESS2)
 
     @classmethod
-    def iniciarThread(cls):
-        cls.setup() # Executa on metodo setup
-        time.sleep(1) # Delay de 1 segundo
-        # Se o atributo t é Vazio cria uma tread
-        if cls.t == None:
-            cls.t = threading.Thread(target=cls.threadLogica)
-        # Verifica se a tread não esta em execucao e se ainda nao foi concluida
-        if cls.t.isAlive() == False and cls.concluida == False:
-            cls.leituraSensores = [] # Limpa a variavel de leituras
-            cls.duracao_total = None # Limpa a varivel de contagem
-            cls.tempo_inicial = time.time() # Defini o tempo incial da logica
-            cls.t.start()
-        else:
-            print('Logica 4 ja concluida, é necessario reicia-la para executar novamente.')
+    def abrirBusto(cls):
+        # Implementar
+        pass
 
-    @classmethod
-    def reiniciarThread(cls):
-        if cls.t != None:
-            if cls.t.isAlive() == True:
-                cls.concluida = True # Finaliza a tread em execucao
-                time.sleep(2)
-        cls.concluida = False
-        cls.leituraSensores = [] # Limpa a variavel de leituras
-        cls.duracao_total = None # Limpa a varivel de contagem
-        cls.tempo_inicial = time.time() # Defini o tempo incial da logica
-        cls.t = threading.Thread(target=cls.threadLogica) # NOVA THREAD
-        cls.iniciarThread()
-
-    @classmethod
-    def thread_isAlive(cls):
-        status = None
-        if not cls.t == None: # Se o atributo t é diferente de Vazio
-            status = cls.t.isAlive()
-        return status
-
+    # Sobreescrevendo metodo threadLogicas() da classe pai
     @classmethod
     def threadLogica(cls):
         while cls.concluida == False:
-            leituraSensor = []
-            leituraSensor.append( GPIO.input(15) )
-            leituraSensor.append( GPIO.input(16) )
-            leituraSensor.append( GPIO.input(37) )
-            leituraSensor.append( GPIO.input(13) )
-            cls.leituraSensores = leituraSensor
-            #print('Logica 4 Sensores: ' + str(cls.leituraSensores))
+            # Checa se a logica 3 já foi concluida
+            if Logica_3._concluida == True:
 
-            # Checa se as condicoes dos sensores magneticos foi satisfeita
-            # e se as logicas anteriores foram concluidas
-            if leituraSensor == [1,1,1,1] and Logica_1.concluida == True and Logica_2.concluida == True and Logica_3.concluida == True:
-                # chama o metodo para abrir a gaveta
-                cls.forcarAbrirTeto()
-                cls.concluida = True
-                cls.duracao_total = time.time() - cls.tempo_inicial
-                print('Logica 4 - Finalizada - Tempo: ' + str(cls.duracao_total) + 'segundos')
-            
+                if (GPIO.input(cls.gpio_chave1) == GPIO.HIGH and GPIO.input(cls.gpio_chave2) == GPIO.HIGH) :
+                    cls.abrirBusto()
+
+                if GPIO.input(cls.gpio_botao) == GPIO.HIGH :
+                    cls.abrirPorta()
+
             time.sleep(1)
+        
+        else:
+            print('Logica 4 - Finalizada')
 
 # ------ FIM DA LOGICA 4 ---------
