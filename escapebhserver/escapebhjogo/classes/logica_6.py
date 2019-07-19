5b2e80a9f8b0b8a27aa9a3ef577191b1d1d6ce70
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2019-07-19 14:29:38 -0300

    Ajuste de tempo Motor 2ª sala

--- escapebhserver/escapebhjogo/classes/logica_6.py
+++ escapebhserver/escapebhjogo/classes/logica_6.py
@@ -38,7 +38,7 @@ class Logica_6(Logica_geral):
         cls._concluida = True
         mcp.setup(cls.gp_motorDescer, mcp.GPB, mcp.OUT, mcp.ADDRESS2)
         mcp.output(cls.gp_motorDescer, mcp.GPB, mcp.LOW, mcp.ADDRESS2)
-        time.sleep(0.5)
+        time.sleep(0.25)
         mcp.output(cls.gp_motorDescer, mcp.GPB, mcp.HIGH, mcp.ADDRESS2)
 
     # Metodo para acionar o motor no sentindo de subir a "gaveta"
@@ -46,7 +46,7 @@ class Logica_6(Logica_geral):
     def subirMotor(cls):
         mcp.setup(cls.gp_motorSubir, mcp.GPB, mcp.OUT, mcp.ADDRESS2)
         mcp.output(cls.gp_motorSubir, mcp.GPB, mcp.LOW, mcp.ADDRESS2)
-        time.sleep(0.5)
+        time.sleep(0.25)
         mcp.output(cls.gp_motorSubir, mcp.GPB, mcp.HIGH, mcp.ADDRESS2)
 
     # Sobreescrevendo metodo threadLogica() da classe pai
