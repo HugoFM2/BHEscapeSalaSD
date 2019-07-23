ae0c642b9547e3a11e0fb8027bc2e96df7169294
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2019-07-23 15:14:04 -0300

    Ajuste do busto - Finalizada a instalação

--- escapebhserver/escapebhjogo/classes/logica_4.py
+++ escapebhserver/escapebhjogo/classes/logica_4.py
@@ -73,14 +73,13 @@ class Logica_4(Logica_geral):
         time.sleep(0.5)
 
         cls.setup()
-        # https://tutorials-raspberrypi.com/raspberry-pi-servo-motor-control/
-        # https://www.embarcados.com.br/pwm-na-raspberry-pi-com-python/
+        #Servo MG90: Pulse Cycle -> 20ms (50Hz), Pulse Width: 0.4ms a 2.4ms (2% a 12%)
         if cls.pwmServo == None:
             cls.pwmServo = GPIO.PWM(cls.gpio_servo, 50) # GPIO inicia PWM de 50HZ, periodo 20ms, no pino do servo
             cls.pwmServo.start(0) # Inicio com DutyCycle em 0%
             time.sleep(0.5)
 
-        cls.pwmServo.ChangeDutyCycle(2.0) # DutyCycle de 2% = Aberto 
+        cls.pwmServo.ChangeDutyCycle(2.5) # Aberto
         time.sleep(1.5)
         
         cls.pwmServo.ChangeDutyCycle(0) # Caso o servo fique tremendo
@@ -92,10 +91,10 @@ class Logica_4(Logica_geral):
 
         if cls.pwmServo == None:
             cls.pwmServo = GPIO.PWM(cls.gpio_servo, 50) # GPIO inicia PWM de 50HZ, periodo 20ms, no pino do servo
-            cls.pwmServo.start(2.5) # Inicio com DutyCycle em 2.5%
+            cls.pwmServo.start(0) # Inicio com DutyCycle em 0%
             time.sleep(0.5)
-        # 12 = Fechado
-        cls.pwmServo.ChangeDutyCycle(12) # DutyCycle de 12% = Travado
+        
+        cls.pwmServo.ChangeDutyCycle(11.4) # Fechado
         time.sleep(1.5)
         cls.pwmServo.ChangeDutyCycle(0) # Caso o servo fique tremendo
         #cls.pwmServo.stop()
