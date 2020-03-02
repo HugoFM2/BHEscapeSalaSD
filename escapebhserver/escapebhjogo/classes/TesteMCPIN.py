14bd4a4d879e5af3382a28dabff2d1881457da03
HugoFM2 <hugofm2@ufmg.br>
2020-03-02 11:05:39 -0300

    comentado testes

--- escapebhserver/escapebhjogo/classes/TesteMCPIN.py
+++ escapebhserver/escapebhjogo/classes/TesteMCPIN.py
@@ -1,9 +1,9 @@
-from mcp23017 import MCP23017 as mcp # Classe para trabalhar com o MCP23017, referenciada como mcp
-import smbus
+# from mcp23017 import MCP23017 as mcp # Classe para trabalhar com o MCP23017, referenciada como mcp
+# import smbus
 
-gpio_botao = 1 # Botao mezanino - GPB 1 (Extensor 0x22)
-mcp.setup(gpio_botao, mcp.GPB, mcp.IN, mcp.ADDRESS1)
-while True:
-	leitura_botao = mcp.input(gpio_botao, mcp.GPB, mcp.ADDRESS1)
-	if(leitura_botao == 1):
-		print("Botao pressionado")
+# gpio_botao = 1 # Botao mezanino - GPB 1 (Extensor 0x22)
+# mcp.setup(gpio_botao, mcp.GPB, mcp.IN, mcp.ADDRESS1)
+# while True:
+# 	leitura_botao = mcp.input(gpio_botao, mcp.GPB, mcp.ADDRESS1)
+# 	if(leitura_botao == 1):
+# 		print("Botao pressionado")
